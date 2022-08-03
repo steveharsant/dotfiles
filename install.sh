19c3bb0657030c2f92aaacdf941d3b015bb1a0f6
Steve Harsant <stevenharsant@gmail.com>
2022-08-03 12:24:03 +0100

    bugfixes

--- install.sh
+++ install.sh
@@ -6,16 +6,16 @@
 script_path="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 ### Bash ###
-dotfiles=$(ls "$script_path" -a1 | grep .bash)
-for dotfile in $dotfiles; do
+dotfiles=( $( ls $script_path -a | grep bash ) )
+for dotfile in "${dotfiles[@]}"; do
   source "$script_path/$dotfile"
 done
 
 cat <<EOL >> /etc/bash.bashrc
 
 # Source personal customisations from github.com/steveharsant/dotfiles
-dotfiles=\$(ls "$script_path" -a1 | grep .bash)
-for dotfile in \$dotfiles; do
+dotfiles=( \$( ls $script_path -a | grep bash ) )
+for dotfile in "\${dotfiles[@]}"; do
   source "$script_path/\$dotfile"
 done
 EOL
@@ -25,3 +25,8 @@ ln -sf "$script_path/.vimrc" /etc/vim/vimrc.local
 
 ### screenrc ###
 echo "SCREENRC='$script_path/.screenrc" >> /etc/environment
+
+dotfiles=( $( ls /srv/dotfiles -a | grep bash ) )
+for dotfile in "${dotfiles[@]}"; do
+  source "/srv/dotfiles/$dotfile"
+done
