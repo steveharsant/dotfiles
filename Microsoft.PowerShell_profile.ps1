3ec617234f989043bdc3214ae8e54023ac398099
Steve Harsant <stevenharsant@gmail.com>
2023-03-19 13:24:09 +0000

    refactor and tidy

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,8 +1,9 @@
-### Shell environment ###
+###* Environment *###
 
 # PSReadLine preferences
 Set-PSReadLineKeyHandler -Key Tab -Function Complete
-Set-PSReadlineKeyHandler -Key ctrl+d -Function ViExit
+Set-PSReadLineKeyHandler -Key ctrl+d -Function ViExit
+Set-PSReadLineKeyHandler -key ctrl+l -Function ClearScreen
 Set-PSReadLineOption -ShowToolTips
 Set-PSReadLineOption -BellStyle None
 
@@ -22,58 +23,82 @@ Set-PSReadLineOption -Color @{
   'Comment'   = [ConsoleColor]::DarkCyan
 }
 
-# Remove curl alias to enable curl for Windows (Silently continue as pwsh7 does not have this alias)
-if ( Get-Command curl -ErrorAction SilentlyContinue ) { Remove-Item alias:curl -ErrorAction SilentlyContinue }
+# Remove curl alias to enable curl for Windows.
+# Silently continue as pwsh7 does not have this alias
+Remove-Item alias:curl -ErrorAction SilentlyContinue
+
+###* Functions *###
+
+function ConvertFrom-Base64 {
+  param([Parameter(ValueFromPipeline)] $in)
+  return [System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String($in))
+}
 
 
-### Simple aliases ###
-New-Alias -Name wh -Value Write-Host
-New-Alias -Name which -Value get-command
-New-Alias -Name wo -Value Write-Output
+function ConvertTo-Base64 {
+  param([Parameter(ValueFromPipeline)] $in)
+  return [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($in))
+}
 
 
-### Functions ###
+function Get-PersistentHistory {
+  param(
+    [Parameter(Position = 0)][int] $head,
+    [Parameter(Position = 0)][int] $tail
+  )
 
-function Get-PersistentHistory ($head, $tail) {
   if (![string]::IsNullOrEmpty($head) -and ![string]::IsNullOrEmpty($tail))
   { Write-Output 'Conflicting arguements passed. Pass either head or tail, no both.'; return }
-  $historyFilePath = "C:\Users\$env:UserName\appdata\Roaming\Microsoft\Windows\PowerShell\PSReadLine\ConsoleHost_history.txt"
+
+  $historyFilePath = "$env:HOMEDRIVE\$env:HOMEPATH\appdata\Roaming\Microsoft\Windows\PowerShell\PSReadLine\ConsoleHost_history.txt"
   if ( ! (Test-Path -Path $historyFilePath) ) { return }
+
   elseif ( ![string]::IsNullOrEmpty($head) ) { return Get-Content $historyFilePath | Select-Object -First $head }
   elseif ( ![string]::IsNullOrEmpty($tail) ) { return Get-Content $historyFilePath | Select-Object -Last $tail }
   else { return Get-Content $historyFilePath }
-}; Remove-Item alias:history -Force; New-Alias history -Value Get-PersistentHistory
+}
+
+Remove-Item alias:history -Force
+New-Alias history -Value Get-PersistentHistory
 
 
-function tldr ([Parameter(Position = 1)]$source = 'tldr', [Parameter(Position = 0)]$command) {
+function Invoke-HistorySearch {
+  param ([Parameter(Position = 0)][string] $search)
+  return Get-PersistentHistory | Where-Object { $_ -match $search }
+}
+
+New-Alias -Name search -Value Invoke-HistorySearch
+
+
+function Invoke-Tldr {
+  param(
+    [Parameter(Position = 0)]         $command,
+    [Parameter(Position = 1)][switch] $cheat
+  )
+  if ($cheat.IsPresent) { $source = 'cheat' } else { $source = 'tldr' }
   (Invoke-WebRequest -Uri "https://cheat.sh/$source`:$command").content
 }
 
-
-function Get-VmIpAddress {
-  (Get-VM | Where-Object { $_.state -eq 'Running' } | Get-VMNetworkAdapter) |
-  Where-Object { $_.ipAddresses -like '*172.*' -or $_.ipAddresses -like '*192.*' } |
-  Select-Object VMName, IpAddresses
-}
+New-Alias -Name tldr -Value Invoke-Tldr
 
 
 function New-Symlink {
   param (
-    [Parameter(Mandatory = $true, Position = 0)][String[]] $objectPath,
-    [Parameter(Mandatory = $true, Position = 1)][String[]] $symlinkPath
-  ) ; New-Item -Type SymbolicLink -Path $symlinkPath -Value $objectPath
-} ; New-Alias -Name ln -Value New-Symlink
+    [Parameter(Position = 0)][String[]] $objectPath,
+    [Parameter(Position = 1)][String[]] $symlinkPath
+  )
+  New-Item -Type SymbolicLink -Path $symlinkPath -Target $objectPath
+}
+
+New-Alias -Name ln -Value New-Symlink
 
 
 function Remove-Object ($path) {
   Remove-Item $path -Recurse -Confirm:$false -Force
-}; Remove-Item Alias:rm -Force; New-Alias rm Remove-Object
-
-
-function Set-NugetAuthentication ( $username = $env:GITHUB_USER, $token = $env:GITHUB_AUTH_TOKEN ) {
-  return New-Object System.Management.Automation.PSCredential -ArgumentList $username,
-    (ConvertTo-SecureString -AsPlainText $token -Force)
 }
+
+Remove-Item Alias:rm -Force
+New-Alias rm Remove-Object
 
 
 function Set-ParentLocation ($path) {
@@ -82,24 +107,38 @@ function Set-ParentLocation ($path) {
   $fullPath -match "(.*$path[^\\]*)" | Out-Null
   if ($fullPath -eq $Matches[0]) { Write-Output 'Unable to guess the path. Be more specific' }
   else { Set-Location $Matches[0] }
-}; New-Alias bd Set-ParentLocation
+}
+
+New-Alias -Name bd -Value Set-ParentLocation
+
+
+function Set-PersistentEnvVar($key, $value) {
+  [System.Environment]::SetEnvironmentVariable($key, $value, 'User')
+}
+New-Alias -Name setenv -Value Set-PersistentEnvVar
 
 
 function Watch-Command ($command) {
   while ($true) { Clear-Host; & $command; Start-Sleep 1 }
-}; New-Alias watch Watch-Command
+}
+
+New-Alias -Name watch -Value Watch-Command
 
 
-### Advanced aliases (Functions) ###
+###* Simple Aliases *###
+
+New-Alias -Name wh -Value Write-Host
+New-Alias -Name which -Value get-command
+New-Alias -Name wo -Value Write-Output
+
+
+###* Advanced Aliases (functions) *###
 
 # General
-function ll { Get-ChildItem -Force $args }
-function find { Get-ChildItem -Path . -Recurse -ErrorAction SilentlyContinue -Force -Filter "*$args*" }
 function mkcd ($path) { New-Item -ItemType Directory -Path $path; Set-Location $path | Out-Null }
 function pg { ping google.com }
 function pgd { ping 8.8.8.8 }
-function seft { wsl seft $args } # https://github.com/steveharsant/seft
-# function tldr { wsl tldr $args } # Alternative native package is nodejs. Install with `npm install tldr -g`
+function py { python3 $args }
 function touch ($path) { New-Item -ItemType File -Path $path | Out-Null }
 function xip { (Invoke-WebRequest 'https://api.ipify.org').content }
 
@@ -119,43 +158,28 @@ function dki { docker.exe image $args }
 function dkils { docker.exe image ls $args }
 function dkix { docker.exe run -it $args }
 
-# Other applications
-function py { python.exe $args }
+# Octopus Deploy
+# Functions to emulate Octopus Deploy functions so copy/pasting from scripts do not error
+function Update-Progress { Write-Host $args -ForegroundColor Black -BackgroundColor Green }
+function Write-Highlight { Write-Host $args -ForegroundColor White -BackgroundColor Blue }
+
+# Hashicorp
 function tf { terraform.exe $args }
-function ts { tailscale.exe $args }
 function vg { vagrant.exe $args }
-function vi { vim.bat $args }
+function vl { vault.exe $args }
 
 
-### directory shortcuts ###
+###* Directory Shortcuts *###
+
 function cdd { Set-Location C:\Users\$ENV:USERNAME\Development }
+function cdr { Set-Location C:\Users\$ENV:USERNAME\Repositories }
 function cdt { Set-Location T:\ } # temp drive
 
 
-### Autocomplete ###
-
-# Octopus CLI autocomplete
-Register-ArgumentCompleter -Native -CommandName octo -ScriptBlock {
-  param($wordToComplete, $commandAst, $cursorPosition)
-  $parms = $commandAst.ToString().Split(' ') | Select-Object -Skip 1
-  octo complete $parms | ForEach-Object {
-    [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterName', $_)
-  }
-}
-
-
-### Additional Profiles ###
+###* Additional Profiles *###
 
 # Chocolatey profile
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
   Import-Module "$ChocolateyProfile"
 }
-
-# Work profile
-$workProfile = "$( Split-Path -Parent $profile )\Set-WorkProfile.ps1"
-if ( Test-Path -Path $workProfile ) {
-  . $workProfile
-}
-
-Clear-Host
